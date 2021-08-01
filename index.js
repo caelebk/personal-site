017702467096a15bf778de0ff029bad1f3f5e847
caeleb <khocaeleb@gmail.com>
2021-08-02 02:55:07 +0800

    fixed port

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) =>{
     res.render('home.ejs');
 })
 
-app.listen(process.env.port || 3000, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
     console.log("Listening on port 3000");
 })
