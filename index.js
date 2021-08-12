06f39da7f606c16ed6d8ba48cdf671e2087cc991
Caeleb <khocaeleb@gmail.com>
2021-08-12 16:13:43 -0700

    updated all the links & added new projects page

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'/views'));
+app.set('views', path.join(__dirname, '/views'));
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.render('home.ejs');
 })
-app.get('/about', (req, res) =>{
+app.get('/about', (req, res) => {
     res.render('about.ejs');
 })
+app.get('/projects', (req, res) => {
+    res.render('projects.ejs');
+})
 
-app.listen(process.env.PORT || 3000, ()=> {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on port 3000");
 })
